dcf7c79840af3b7ebd29feaff048e899ebcae953
tanner-johnson2718 <tanner.johnson2718@gmail.com>
2021-12-04 23:36:51 -0900

    can put and recv data on serial, not sure its inited and needs some work

--- README.txt
+++ README.txt
@@ -2,3 +2,5 @@
 First MEME OS repository will be used to examine the Linux kernel. This project
 will be the actual creation of an OS from as scratch as possible. For now
 target will be the x86-64 architecture and will target the qemu virtual env.
+
+# Boot Process
--- kernel_entry.c
+++ kernel_entry.c
@@ -1,10 +1,48 @@
 // Entry point for the kernel
 
-#include "kernel_entry.h"
+extern int kernel_start;
+extern int kernel_end;
 
-void kernel_main(void) {
-    int i = 0;
+#define COM1            0x3F8   // 
+#define COM_LSR		    5	    // In: Line Status Register
+#define COM_LSR_DATA	0x01	//     Data available
+#define COM_LSR_TXRDY	0x20	//     Transmit buffer avail
+#define COM_LSR_TSRE	0x40	//     Transmitter off
+
+static inline void outb(int port, char data)
+{
+	asm volatile("outb %0,%w1" : : "a" (data), "d" (port));
+}
+
+static inline char inb(int port)
+{
+	char data;
+	asm volatile("inb %w1,%0" : "=a" (data) : "d" (port));
+	return data;
+}
+
+void serial_putc(char data)
+{
+    outb(COM1, data);
+}
+
+char serial_getc()
+{
+    // wait till data rx line set high
+    char rdy = 0;
+    while(!rdy){
+        rdy = inb((COM1 + COM_LSR) & COM_LSR_DATA);
+        serial_putc(rdy + 60);
+    }
+
+    return inb(COM1);
+
+}
+
+void kernel_main(void) 
+{
     for(;;){
-        ++i;
+        char c = serial_getc();
+        serial_putc(c);
     }
 }
