a2bbaf67e1e8fd64084719ec51079b8c62cf513b
tanner-johnson2718 <tanner.johnson2718@gmail.com>
2021-12-08 19:56:11 -0900

    ..

--- irq.h
+++ irq.h
@@ -1,6 +1,14 @@
 #ifndef MEME_IRQ
 #define MEME_IRQ
 
+#define IRQ_EOI_MASTER_PORT  0x20
+#define IRQ_EOI_SLAVE_PORT   0xA0
+#define IRQ_EOI_MSG          0x20
+#define IRQ_MASK_MASTER_PORT 0x21
+#define IRQ_MASK_SLAVE_PORT  0x21
 
+void irq_reg_isr(int irq_num);
+void irq_send_eoi(int irq_num);
+void irq_toggle_mask(int irq_num);
 
 #endif
