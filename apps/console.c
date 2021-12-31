34e7ac8b0bd9fa2628e92dc51e3dd6a279e73572
tanner-johnson2718 <tanner.johnson2718@gmail.com>
2021-12-30 19:16:52 -0900

    console scructure done, needs to be filld out

--- apps/console.c
+++ apps/console.c
@@ -34,12 +34,80 @@ u8 fg = VGA_WHITE;
 u8 row = 0;
 u8 col = 0;
 
-void ps2_to_vga_full()
+#define MAX_LINE_SIZE 256
+u8 index = 0;
+u8 line_buffer[MAX_LINE_SIZE];
+
+void output_c(u8 c)
+{
+    vga_msg_t msg;
+    msg.bg = bg;
+    msg.fg = fg;
+    msg.col = col;
+    msg.row = row;
+    msg.c = c;
+
+    sched_app_publish_OUT_event((u8*) &msg, sizeof(vga_msg_t), SCHED_VGA_ID);
+}
+
+void output_s()
+{
+    
+}
+
+void clr_scr()
+{
+
+}
+
+void update_screen_index(u8 c)
+{
+    col++;
+    if(col == VGA_BUFFER_WIDTH)
+    {
+        row++;
+        col = 0;
+    }
+
+    if(row == VGA_BUFFER_HEIGHT)
+    {
+        row = 0;
+    }
+
+    if(c == '\n')
+    {
+        row++;
+        col = 0;
+        if(row == VGA_BUFFER_HEIGHT)
+        {
+            row = 0;
+        }
+    }
+   
+}
+
+void insert_line_buffer(u8 c)
+{
+    if(index >= MAX_LINE_SIZE)
+    {
+        // BIG OOF, what to do?
+    }
+
+    line_buffer[index] = c;
+    index++;
+}
+
+void process_line_buffer()
+{
+
+}
+
+void input_handler()
 {
     u8 size = EVENT_DATA_SIZE;
     u8 data[size];
     u8 num_read = 0;
-    vga_msg_t msg;
+    
 
     num_read =  sched_app_pop_IN_event(SCHED_PS2_ID, data, size);
 
@@ -47,41 +115,22 @@ void ps2_to_vga_full()
     u8 i = 0;
     for(i = 0; i < num_read; ++i)
     {
+
+        output_c(data[i]);
+        update_screen_index(data[i]);
+
         if(data[i] == '\n')
         {
-            row++;
-            if(row == VGA_BUFFER_HEIGHT)
-            {
-                row = 0;
-                col = 0;
-            }
-
-            continue;
+            process_line_buffer();
         }
-
-        msg.bg = bg;
-        msg.fg = fg;
-        msg.col = col;
-        msg.row = row;
-        msg.c = data[i];
-
-        sched_app_publish_OUT_event((u8*) &msg, sizeof(vga_msg_t), SCHED_VGA_ID);
-        
-        col++;
-        if(col == VGA_BUFFER_WIDTH)
+        else
         {
-            row++;
-            col = 0;
-        }
-
-        if(row == VGA_BUFFER_HEIGHT)
-        {
-            row = 0;
+            insert_line_buffer(data[i]);
         }
     }
 }
 
 void console_init()
 {
-    sched_app_register_callback(ps2_to_vga_full);
+    sched_app_register_callback(input_handler);
 }
