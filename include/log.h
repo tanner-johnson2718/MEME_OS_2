026346fb36e369a8354dd0a67850ddb02a6aac0a
tanner-johnson2718 <tanner.johnson2718@gmail.com>
2022-01-05 12:41:57 -0900

    commiting unfinished log code

--- include/log.h
+++ include/log.h
@@ -6,11 +6,8 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 // Kernel and user space logging library. Will provide functions for logging
-// and error output to the serial console. There will be 3 debug levels, log
-// is just a message send to the user, warning is a warning that something is
-// potentailly not right, and error is a serious error report. Each level will
-// have its own logging function. Moreover the log message will contain the
-// the level it was logged.
+// and error output to the serial console. Will allow for passing of the file
+// and line macro. 
 
 ///////////////////////////////////////////////////////////////////////////////
 // Kernel Public Macros
@@ -26,7 +23,7 @@ NAME)     log_msg
 
 INPUTS)
           0) u8* file  - The __FILE__ macro in c-string format
-          1) u8* file  - The __LINE__ macro in c-string format
+          1) u32 file  - The __LINE__ macro in integer format
           2) u8* c_str - A pointer to a c_str i.e. null terminated string to
                          output at a 'error' level
 
@@ -34,46 +31,8 @@ OUTPUTS)  NONE
 
 RETURNS)  0 on sucess and valid error code on error
 
-COMMENTS) NONE
+COMMENTS) If the resultant log is >255 bytes, message will be truncated
 ******************************************************************************/
-u8 log_msg(u8* file, u8* line, u8* c_str);
-
-
-
-/******************************************************************************
-NAME)     log_warning
-
-INPUTS)
-          0) u8* file  - The __FILE__ macro in c-string format
-          1) u8* file  - The __LINE__ macro in c-string format
-          2) u8* c_str - A pointer to a c_str i.e. null terminated string to
-                         output at a 'error' level
-
-OUTPUTS)  NONE
-
-RETURNS)  0 on sucess and valid error code on error
-
-COMMENTS) NONE
-******************************************************************************/
-u8 log_warning(u8* file, u8* line, u8* c_str);
-
-
-
-/******************************************************************************
-NAME)     log_error
-
-INPUTS)
-          0) u8* file  - The __FILE__ macro in c-string format
-          1) u8* file  - The __LINE__ macro in c-string format
-          2) u8* c_str - A pointer to a c_str i.e. null terminated string to
-                         output at a 'error' level
-
-OUTPUTS)  NONE
-
-RETURNS)  0 on sucess and valid error code on error
-
-COMMENTS) NONE
-******************************************************************************/
-u8 log_error(u8* file, u8* line, u8* c_str);
+u8 log_msg(u8* file, u32 line, u8* c_str);
 
 #endif
--- kernel/log.c
+++ kernel/log.c
@@ -1,6 +1,7 @@
 #include "types.h"
 #include "log.h"
 #include "serial.h"
+#include "timer.h"
 
 // See log.h for "high level" documentation
 
@@ -8,13 +9,104 @@
 // Private Register and Macros
 ///////////////////////////////////////////////////////////////////////////////
 
-#define LOG_MSG_STR   "MSG"
-#define LOG_WARN_STR  "WARNING"
-#define LOG_ERROR_STR "ERROR"
+#define LOG_MAX_MSG_LEN 255        // 256-1 to fit '\0'
 
 ///////////////////////////////////////////////////////////////////////////////
 // Private Functions
 ///////////////////////////////////////////////////////////////////////////////
+
+// assumes enough room, access a max of 10 bytes
+u8 dtos(u32 d, u8* num)
+{
+    u32 m = 10;
+    num[0] = 0;
+    u32 i;
+    for(i = 0; (i<m); ++i)
+    {
+        num[i] = d % 10;
+        d = d / 10;
+
+        if(d == 0)
+        {
+            break;
+        }
+    }
+
+    return i;
+}
+
+u8 place_in_buffer(u8 i, u8* buffer, u8* str)
+{
+    u8 j = 0;
+    while( (i < LOG_MAX_MSG_LEN) && (str[j] != '\0') )
+    {
+        buffer[i] = str[j];
+        ++i;
+        ++j;
+    }
+    
+    return i;
+}
+
+// Expects buffer to be of len 256
+u8 build_message(u8* buffer, u8* file, u32 line, u8* msg)
+{
+    u32 time = 0;
+    timer_get_time_ms(&time);
+
+    u8 i = 0;
+
+    // Put [time]
+    buffer[i] = '[';
+    ++i;
+    i+= dtos(time, buffer + i);
+    buffer[i] = ']';
+    ++i;
+
+    // Put [file], need to start checking i during file name insert
+    buffer[i] = '[';
+    ++i;
+
+    i = place_in_buffer(i,buffer,file);
+    if(i == LOG_MAX_MSG_LEN)
+    {
+        return i;
+    }
+
+    buffer[i] = ']';
+    ++i;
+    if(i == LOG_MAX_MSG_LEN)
+    {
+        return i;
+    }
+
+    // Put [line]
+    buffer[i] = '[';
+    ++i;
+    if(i == LOG_MAX_MSG_LEN)
+    {
+        return i;
+    }
+
+    u8 line_str[11];
+    u8 line_index = dtos(line, line_str);
+    line_str[line_index] = '\0';
+    i = place_in_buffer(i,buffer,line_str);
+    if(i == LOG_MAX_MSG_LEN)
+    {
+        return i;
+    }
+
+    buffer[i] = ']';
+    ++i;
+    if(i == LOG_MAX_MSG_LEN)
+    {
+        return i;
+    }
+    
+    // Put " <msg>"
+    
+}
 
 ///////////////////////////////////////////////////////////////////////////////
 // Kernel Public API Functions
@@ -25,7 +117,7 @@ NAME)     log_msg
 
 INPUTS)
           0) u8* file  - The __FILE__ macro in c-string format
-          1) u8* file  - The __LINE__ macro in c-string format
+          1) u32 line  - The __LINE__ macro in integer format
           2) u8* c_str - A pointer to a c_str i.e. null terminated string to
                          output at a 'error' level
 
@@ -33,53 +125,9 @@ OUTPUTS)  NONE
 
 RETURNS)  0 on sucess and valid error code on error
 
-COMMENTS) NONE
+COMMENTS) If the resultant log is >255 bytes, message will be truncated
 ******************************************************************************/
-u8 log_msg(u8* file, u8* line, u8* c_str)
-{
-
-}
-
-
-
-/******************************************************************************
-NAME)     log_warning
-
-INPUTS)
-          0) u8* file  - The __FILE__ macro in c-string format
-          1) u8* file  - The __LINE__ macro in c-string format
-          2) u8* c_str - A pointer to a c_str i.e. null terminated string to
-                         output at a 'error' level
-
-OUTPUTS)  NONE
-
-RETURNS)  0 on sucess and valid error code on error
-
-COMMENTS) NONE
-******************************************************************************/
-u8 log_warning(u8* file, u8* line, u8* c_str)
-{
-
-}
-
-
-
-/******************************************************************************
-NAME)     log_error
-
-INPUTS)
-          0) u8* file  - The __FILE__ macro in c-string format
-          1) u8* file  - The __LINE__ macro in c-string format
-          2) u8* c_str - A pointer to a c_str i.e. null terminated string to
-                         output at a 'error' level
-
-OUTPUTS)  NONE
-
-RETURNS)  0 on sucess and valid error code on error
-
-COMMENTS) NONE
-******************************************************************************/
-u8 log_error(u8* file, u8* line, u8* msg)
+u8 log_msg(u8* file, u32 line, u8* c_str)
 {
 
 }
